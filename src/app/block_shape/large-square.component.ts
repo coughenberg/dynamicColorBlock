e47424eeda494ca236ab91369896b80fb93bfa0a
coughenberg <khfantk@gmail.com>
2020-11-01 15:12:15 -0600

    Refactored edge uncoloring

--- src/app/block_shape/large-square.component.ts
+++ src/app/block_shape/large-square.component.ts
@@ -1,6 +1,6 @@
 import { elementPlacementInterface } from './../elementPlacementInterface';
 import { SquareElement } from './square_element/element-square.component';
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'large-square',
@@ -53,13 +53,14 @@ export class LargeSquareComponent implements OnInit {
         squareElements[i][j] = new SquareElement();
         squareElements[i][j].heightPlaced = i;
         squareElements[i][j].widthPlaced = j;
-        squareElements[i][j].clicked = this.squareElements && this.squareElements[i] && this.squareElements[i][j] && this.squareElements[i][j].clicked ? this.squareElements[i][j].clicked : false;
+        squareElements[i][j].clicked = false;
+        // squareElements[i][j].clicked = this.squareElements && this.squareElements[i] && this.squareElements[i][j] && this.squareElements[i][j].clicked ? this.squareElements[i][j].clicked : false;
       }
     }
 
     this.squareElements = squareElements;
-    let upperLeftMostTile: elementPlacementInterface = {height: LargeSquareComponent.TOTAL_HEIGHT, width: LargeSquareComponent.TOTAL_WIDTH};
-    if(changeArray && changeArray.length > 0) {
+    let upperLeftMostTile: elementPlacementInterface = { height: LargeSquareComponent.TOTAL_HEIGHT, width: LargeSquareComponent.TOTAL_WIDTH };
+    if (changeArray && changeArray.length > 0) {
       changeArray.forEach(change => {
         this.squareElements[change.height][change.width].parentClick = true;
         if (change.height <= upperLeftMostTile.height) {
@@ -81,12 +82,12 @@ export class LargeSquareComponent implements OnInit {
    */
   colorOutterSides(upperLeftMostTile: elementPlacementInterface): elementPlacementInterface {
     this.parentClickSquaresToFalsify = [];
-    const leftOfUpperLeftMostTile: elementPlacementInterface = {height: upperLeftMostTile.height, width: upperLeftMostTile.width - 1};
+    const leftOfUpperLeftMostTile: elementPlacementInterface = { height: upperLeftMostTile.height, width: upperLeftMostTile.width - 1 };
     let lastTileFilled: elementPlacementInterface;
     lastTileFilled = this.colorThisSide(leftOfUpperLeftMostTile.height, leftOfUpperLeftMostTile.width, false, true, leftOfUpperLeftMostTile.height, leftOfUpperLeftMostTile.width + 1);
     lastTileFilled = this.colorThisSide(lastTileFilled.height, lastTileFilled.width, true, true, lastTileFilled.height - 1, lastTileFilled.width);
     lastTileFilled = this.colorThisSide(lastTileFilled.height, lastTileFilled.width, false, false, lastTileFilled.height, lastTileFilled.width - 1);
-    lastTileFilled = this.colorThisSide(lastTileFilled.height, lastTileFilled.width, true, false, lastTileFilled.height  + 1, lastTileFilled.width);
+    lastTileFilled = this.colorThisSide(lastTileFilled.height, lastTileFilled.width, true, false, lastTileFilled.height + 1, lastTileFilled.width);
     const lastSquare = this.parentClickSquaresToFalsify.pop();
     this.parentClickSquaresToFalsify.forEach(square => {
       this.squareElements[square.height][square.width].parentClick = false;
@@ -107,34 +108,119 @@ export class LargeSquareComponent implements OnInit {
    */
   colorThisSide(currHeight: number, currWidth: number, isHorizontalMoving: boolean, isPositiveIncrement: boolean, connectedHeight: number, connectedWidth: number): elementPlacementInterface {
     let first = true;
-    while (this.squareElements[currHeight] && this.squareElements[currHeight][currWidth] && (this.squareElements[connectedHeight][connectedWidth].parentClick || (this.origin.height === connectedHeight && this.origin.width === connectedWidth))) {
-      this.squareElements[currHeight][currWidth].parentClick = true;
-      if (!first) {
-        this.parentClickSquaresToFalsify.push({
-          height: connectedHeight,
-          width: connectedWidth
-        });
+    let lastSquare: elementPlacementInterface;
+    if (currHeight < 0 || currHeight > LargeSquareComponent.TOTAL_HEIGHT - 1 || currWidth < 0 || currWidth > LargeSquareComponent.TOTAL_WIDTH - 1) {
+      lastSquare = this.uncolorThisSide(isHorizontalMoving, isPositiveIncrement, connectedHeight, connectedWidth);
+    } else {
+      while (this.squareElements[currHeight] && this.squareElements[currHeight][currWidth] && (this.squareElements[connectedHeight][connectedWidth].parentClick || (this.origin.height === connectedHeight && this.origin.width === connectedWidth || first))) {
+        this.squareElements[currHeight][currWidth].parentClick = true;
+        if (!first) {
+          this.parentClickSquaresToFalsify.push({
+            height: connectedHeight,
+            width: connectedWidth
+          });
+        }
+        first = false;
+        if (isHorizontalMoving) {
+          if (isPositiveIncrement) {
+            currWidth++;
+            connectedWidth++;
+          } else {
+            currWidth--;
+            connectedWidth--;
+          }
+        } else {
+          if (isPositiveIncrement) {
+            currHeight++;
+            connectedHeight++;
+          } else {
+            currHeight--;
+            connectedHeight--;
+          }
+        }
+        lastSquare = { height: currHeight, width: currWidth };
       }
-      first = false;
+    }
+    return lastSquare;
+  }
+
+  /**
+   * Uncolors the given side
+   * 
+   * @param isHorizontalMoving - Whether the outline is currently coloring in horizontally or vertically
+   * @param isPositiveIncrement - Whether the outline is currently coloring in the positive or negative direction
+   * @param connectedHeight - The row for the connected tile to check if it is colored
+   * @param connectedWidth - The column for the connected tile to check if it is colored
+   */
+  uncolorThisSide(isHorizontalMoving: boolean, isPositiveIncrement: boolean, connectedHeight: number, connectedWidth: number): elementPlacementInterface {
+    let nextBlock: number;
+    while (this.squareElements[connectedHeight] && this.squareElements[connectedHeight][connectedWidth] && this.getNextCheckedBlock(connectedHeight, connectedWidth, isHorizontalMoving, nextBlock)) {
       if (isHorizontalMoving) {
         if (isPositiveIncrement) {
-          currWidth++;
           connectedWidth++;
+          nextBlock = connectedWidth + 1;
         } else {
-          currWidth--;
           connectedWidth--;
+          nextBlock = connectedWidth - 1;
         }
       } else {
         if (isPositiveIncrement) {
-          currHeight++;
           connectedHeight++;
+          nextBlock = connectedHeight + 1;
         } else {
-          currHeight--;
           connectedHeight--;
+          nextBlock = connectedHeight - 1;
         }
       }
+      if (this.edgeOfLargeSquare(connectedHeight, connectedWidth, nextBlock, isHorizontalMoving)) {
+        this.squareElements[connectedHeight][connectedWidth].parentClick = false;
+      }
     }
-    return {height: currHeight, width: currWidth};
+    if (isHorizontalMoving) {
+      if (isPositiveIncrement) {
+        connectedWidth++;
+        nextBlock = connectedWidth + 1;
+      } else {
+        connectedWidth--;
+        nextBlock = connectedWidth - 1;
+      }
+    } else {
+      if (isPositiveIncrement) {
+        connectedHeight++;
+        nextBlock = connectedHeight + 1;
+      } else {
+        connectedHeight--;
+        nextBlock = connectedHeight - 1;
+      }
+    }
+    return { height: connectedHeight, width: connectedWidth };
+  }
+
+  /**
+   * Whether the square is touching the corner of the larger square or not
+   * 
+   * @param elementSquareHeight - Height of current square to evaluate
+   * @param elementSquareWidth - Width of current square to evaluate
+   */
+  edgeOfLargeSquare(elementSquareHeight: number, elementSquareWidth: number, nextBlock: number, isHorizontalMoving: boolean): boolean {
+    const endOfMadeSquare: boolean = isHorizontalMoving ? this.squareElements[elementSquareHeight][nextBlock] && this.squareElements[elementSquareHeight][nextBlock].parentClick :
+      this.squareElements[nextBlock] && this.squareElements[nextBlock][elementSquareWidth] && this.squareElements[nextBlock][elementSquareWidth].parentClick;
+    // return elementSquareHeight <= 0 && elementSquareWidth >= LargeSquareComponent.TOTAL_WIDTH - 1 ||
+    //   elementSquareHeight <= 0 && elementSquareWidth <= 0 ||
+    //   elementSquareWidth <= 0 && elementSquareHeight >= LargeSquareComponent.TOTAL_HEIGHT - 1 ||
+    //   elementSquareWidth <= 0 && elementSquareHeight <= 0 ||
+    //   elementSquareWidth >= LargeSquareComponent.TOTAL_WIDTH - 1 && elementSquareHeight >= LargeSquareComponent.TOTAL_HEIGHT - 1;
+    return elementSquareHeight <= 0 || elementSquareHeight >= LargeSquareComponent.TOTAL_HEIGHT - 1 ||
+      elementSquareWidth <= 0 || elementSquareWidth >= LargeSquareComponent.TOTAL_WIDTH - 1;
+
+  }
+
+  getNextCheckedBlock(elementSquareHeight: number, elementSquareWidth: number, isHorizontalMoving: boolean, nextBlock: number) {
+    if (nextBlock == null) {
+      return true;
+    }
+    return isHorizontalMoving ? this.squareElements[elementSquareHeight][nextBlock] && this.squareElements[elementSquareHeight][nextBlock].parentClick :
+      this.squareElements[nextBlock] && this.squareElements[nextBlock][elementSquareWidth] && this.squareElements[nextBlock][elementSquareWidth].parentClick;
   }
 
   /**
@@ -143,14 +229,14 @@ export class LargeSquareComponent implements OnInit {
    * @param elementClicked - The button that has been pressed 
    */
   displayClickedFromParent(elementClicked: SquareElement) {
-    console.warn(elementClicked);
-    elementClicked.parentClick = !elementClicked.parentClick;
     this.squareElements[elementClicked.heightPlaced][elementClicked.widthPlaced].clicked = true;
-    this.origin = {height: elementClicked.heightPlaced, width: elementClicked.widthPlaced};
-    let nextUpperLeftPosition: elementPlacementInterface;
-    nextUpperLeftPosition = this.colorOutterSides({ height: elementClicked.heightPlaced, width: elementClicked.widthPlaced });
-    for (let index = 1; index < LargeSquareComponent.TOTAL_HEIGHT; index++) {
+    this.origin = { height: elementClicked.heightPlaced, width: elementClicked.widthPlaced };
+    let nextUpperLeftPosition: elementPlacementInterface = { height: elementClicked.heightPlaced, width: elementClicked.widthPlaced };
+    for (let index = 1; index < LargeSquareComponent.TOTAL_HEIGHT + 1; index++) {
       setTimeout(() => {
+        if (index === 1) {
+          this.squareElements[elementClicked.heightPlaced][elementClicked.widthPlaced].clicked = false;
+        }
         nextUpperLeftPosition = this.colorOutterSides(nextUpperLeftPosition);
       }, index * 600);
     }
