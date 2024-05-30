1d7fa7a6cc57bb78a682d1a1e42726021994061d
coughenberg <khfantk@gmail.com>
2024-05-30 00:11:29 -0500

    feat: upgrade angular to version 17

--- src/app/block_shape/square_element/element-square.component.ts
+++ src/app/block_shape/square_element/element-square.component.ts
@@ -1,49 +1,50 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'element-square',
-    templateUrl: 'element-square.component.html',
-    styleUrls: ['./element-square.component.css']
+  selector: 'element-square',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: 'element-square.component.html',
+  styleUrls: ['./element-square.component.css'],
 })
-
 export class SquareElement implements OnInit {
-    /**
-     * Height the element was placed
-     */
-    @Input()
-    heightPlaced: number;
+  /**
+   * Height the element was placed
+   */
+  @Input()
+  heightPlaced: number = 0;
 
-    /**
-     * Width the element was placed
-     */
-    @Input()
-    widthPlaced: number;
+  /**
+   * Width the element was placed
+   */
+  @Input()
+  widthPlaced: number = 0;
 
-    /**
-     * True if the parent class has clicked this element
-     */
-    @Input()
-    parentClick: boolean;
+  /**
+   * True if the parent class has clicked this element
+   */
+  @Input()
+  parentClick: boolean = false;
 
-    /**
-     * Whether the button was clicked or not
-     */
-    @Input()
-    clicked: boolean = false;
+  /**
+   * Whether the button was clicked or not
+   */
+  @Input()
+  clicked: boolean = false;
 
-    /**
-     * Tells parent which element has been clicked
-     */
-    @Output()
-    newClick = new EventEmitter<Object>();
+  /**
+   * Tells parent which element has been clicked
+   */
+  @Output()
+  newClick = new EventEmitter<SquareElement>();
 
-    constructor() { }
+  constructor() {}
 
-    ngOnInit() {
-    }
+  ngOnInit() {}
 
-    givePlacement(): void {
-        this.clicked = !this.clicked;
-        this.newClick.emit(this);
-    }
+  givePlacement(): void {
+    this.clicked = !this.clicked;
+    this.newClick.emit(this);
+  }
 }
